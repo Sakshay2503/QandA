f51b5bd4f230ff61489297dacc835495c8b1118c
Akshay Pawar <127084513+Sakshay2503@users.noreply.github.com>
2024-02-26 14:46:14 +0530

    Add files via upload

---
+++ model/answerModel.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose")
+
+// Answer Schema
+
+const AnswerSchema = new mongoose.Schema({
+    body:
+    {
+        type:String,
+
+    } ,
+    createdAt: { type: Date, default: Date.now },
+    question: { type: mongoose.Schema.Types.ObjectId, ref: 'Question' }
+  });
+
+  module.exports=AnswerSchema;
